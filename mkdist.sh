bc93218f5c8c8e62641aabc7fe26ea90776548c6
Matthias Reichl <hias@horus.com>
2011-05-28 15:30:43 +0000

    fixed mkdist.sh to include contrib/gentoo

--- mkdist.sh
+++ mkdist.sh
@@ -12,6 +12,7 @@ mkdir -p tmp/"${NAME}"
 mkdir tmp/"${NAME}"/tools
 mkdir tmp/"${NAME}"/tools/6502
 mkdir tmp/"${NAME}"/driver
+mkdir -p tmp/"${NAME}"/contrib/gentoo
 cp -a Changelog* README* INSTALL* LICENSE* Makefile \
 	atarisio-modprobe* atarisio-udev* getver.sh getcurses.sh tmp/"${NAME}"
 cp -a tools/Makefile tools/*.cpp tools/*.h tmp/"${NAME}"/tools
@@ -19,6 +20,7 @@ cp -a tools/6502/Makefile tools/6502/*.src tools/6502/*.inc \
       tools/6502/*.c tools/6502/*.h tools/6502/*.bin \
       tools/6502/remote.com tmp/"${NAME}"/tools/6502
 cp -a driver/Makefile driver/*.c driver/*.h tmp/"${NAME}"/driver
+cp -a contrib/gentoo/* tmp/"${NAME}"/contrib/gentoo/
 cd tmp
 tar zcf ../dist/${NAME}.tar.gz "${NAME}"
 cp -a ../Changelog ../README ../dist
