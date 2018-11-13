62875edf695f767483b365811925154681f2e2e1
Matthias Reichl <hias@horus.com>
2018-11-13 11:42:35 +0100

    update Changelog, bump version
    Signed-off-by: Matthias Reichl <hias@horus.com>

--- tools/Version.h
+++ tools/Version.h
@@ -4,7 +4,7 @@
 /*
    Version.h - define the AtariSIO version string
 
-   Copyright (C) 2003-2015 Matthias Reichl <hias@horus.com>
+   Copyright (C) 2003-2018 Matthias Reichl <hias@horus.com>
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -22,6 +22,6 @@
 */
 
 //#define VERSION_STRING "0.30-pre3"
-#define VERSION_STRING "0.30-181111"
+#define VERSION_STRING "0.30-181113"
 
 #endif
